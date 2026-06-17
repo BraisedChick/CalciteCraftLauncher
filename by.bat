dd2fe4b3b2308556978d8cc67b59b9b73d95f91b
BraisedChick <3548211425@qq.com>
2026-06-17 22:08:59 +0800

    停止追踪工具

--- by.bat
+++
@@ -1,3 +0,0 @@
-.\glslangValidator.exe -V src/main/cpp/shaders/shader.vert -o src/main/assets/shaders/shader_vert.spv
-.\glslangValidator.exe -V src/main/cpp/shaders/shader.frag -o src/main/assets/shaders/shader_frag.spv
-pause
